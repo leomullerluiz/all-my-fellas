26b6a0cb6bd101facc30d52b25e695f8c3a076d9
Leo Müller <leomullerluiz@gmail.com>
2026-08-05 01:19:05 -0300

    Mount ToastContainer once in the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
+import { ToastProvider } from "@/components/toast-provider";
 import { getSettings } from "@/server/settings/store";
 
 import "./globals.css";
@@ -35,7 +36,10 @@ export default function RootLayout({
       data-theme={theme === "system" ? undefined : theme}
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <ToastProvider />
+      </body>
     </html>
   );
 }
